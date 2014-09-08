f6c61b9ec41f9d09830b533d71ac86efe6ac9733
Andrew Rota <rota.andrew@gmail.com>
2014-09-08 22:23:58 +0000

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*spec.js']
+    },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.registerTask('test', ['jshint', 'mochaTest']);
+  grunt.registerTask('default', ['test']);
+};
---
+++ test/main.spec.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+describe('Main Test', function() {
+    it('should be true', function() {
+        assert.ok(true);
+    });
+});
